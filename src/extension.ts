08efb28a589d7f3b7a43a9e9c85da85b222686ab
zhaoshengjun <zhao.shengjun@gmail.com>
2016-05-22 21:48:28 +1000

    add functions

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,31 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+import {NgToolbelt} from './ng2toolbelt';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let toolbelt = new NgToolbelt();
+    let disposable = vscode.commands.registerCommand('extension.ng2toolbelt', () => {
+        let window = vscode.window;
+        let rootPath = vscode.workspace.rootPath;
+        let fileName: string;
+        let typeName: string;                
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ng2toolbelt" is now active!');
+        window.showInputBox({
+            placeHolder: "What name do you want to use?"
+        }).then((filename) => {
+            if (filename) {
+                fileName = filename;
+                window.showQuickPick(['component', 'service', 'pipe','class','directive','route').then((typename) => {
+                    if (typename) {
+                        typeName = typename;
+                        toolbelt.execGenerateCommand(fileName, typeName, rootPath)         
+                    }
+                })
+            }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        })
     });
 
     context.subscriptions.push(disposable);
